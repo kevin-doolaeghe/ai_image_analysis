97593cb6ff95f3133e4d2564f7a825a95895704a
Kevin Doolaeghe <doolaeghe.kevin@hotmail.com>
2023-12-31 01:39:03 +0100

    Tested classification: slow but working. The item found is not correct

--- lib/tflite/web/image_classifier.dart
+++ lib/tflite/web/image_classifier.dart
@@ -2,16 +2,24 @@
 library main;
 
 import 'package:camera/camera.dart';
+import 'package:flutter/services.dart';
+import 'package:image/image.dart' as img;
 import 'package:js/js.dart';
 import 'package:js/js_util.dart' as jsutil;
 
 @JS('classifyImage')
-external String _classifyImage(XFile image);
+external String _classifyImage(Uint8List data, int width, int height);
 
 class ImageClassifier {
   Future<String> classifyImage(XFile image) async {
+    final imageData = await image.readAsBytes();
+    final decodedImage = img.decodeImage(imageData);
     String category = await jsutil.promiseToFuture<String>(
-      _classifyImage(image),
+      _classifyImage(
+        decodedImage!.toUint8List(),
+        decodedImage.width,
+        decodedImage.height,
+      ),
     );
     return category;
   }
--- web/scripts/ml.js
+++ web/scripts/ml.js
@@ -1,19 +1,35 @@
-async function classifyImage(image) {
-    return 'test';
+function imageToTensor(data, width, height) {
+    const buffer = new Uint8Array(width * height * 3)
+    let offset = 0;
+    for (let i = 0; i < buffer.length; i += 3) {
+        buffer[i] = data[offset];
+        buffer[i + 1] = data[offset + 1];
+        buffer[i + 2] = data[offset + 2];
+        offset += 4;
+    }
 
-    /*
-    // Load the MobileNet model
-    const model = await mobilenet.load();
+    return tf.tensor3d(buffer, [height, width, 3]);
+}
 
-    // Classify the image
-    let predictions = await model.classify(image);
-
-    // Extract the result
+async function classifyImage(data, width, height) {
     let result = [];
-    predictions.forEach((item, _) => result.push(item));
+    try {
+        let tensor = imageToTensor(data, width, height);
 
-    return result;
-    */
+        // Load the MobileNet model
+        const model = await mobilenet.load();
+
+        // Classify the image
+        let predictions = await model.classify(tensor);
+
+        // Extract the result
+        predictions.forEach((item, _) => result.push(item));
+    } catch (error) {
+        console.log('Error: ' + error);
+    }
+
+    console.log(result);
+    return result.toString();
 }
 
 async function detectObjects(image) {
